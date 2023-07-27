1a8024bc8631ab85b540534336751dcc12044440
Rishab29-code <rihandev36@gmail.com>
2023-07-27 14:57:52 +0530

    responsive messages

--- src/pages/Chat/Selected User Container/SelectedUserCont.js
+++ src/pages/Chat/Selected User Container/SelectedUserCont.js
@@ -1,23 +1,59 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { useSelector } from 'react-redux'
 import NewChatContainer from '../New Chat Container/NewChatContainer'
 import UserChatContainer from '../User Chat Container/UserChatContainer'
 import UserTitleCont from '../User Title Cont/UserTitleCont'
 import styles from "./SelectedUserCont.module.css"
 
+
+
+
+
 const SelectedUserCont = ({setChatUserData}) => {
   const chatData=useSelector((state)=>state.chatLatest)
+  console.log(chatData.chatdisplay);
+  const [width, setWidth] = useState(window.innerWidth);
+  
+
+  const updateWidth = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWidth);
+    return () => window.removeEventListener("resize", updateWidth);
+  }, []);
+
+
+
+
   return (
     <>
-    <section className={styles.outerCont}>
-    {!chatData.selectedUser&&<><div className={styles.innerCont}>No Chat Selected</div></>}
-    {chatData.selectedUser&&<>
-       <UserTitleCont/>
-       <UserChatContainer/>
-       <NewChatContainer/>
-    </>}
-    </section>
-    </>
+    {width>500?
+      <section className={styles.outerCont}>
+      {!chatData.selectedUser&&<><div className={styles.innerCont}>No Chat Selected</div></>}
+      {chatData.selectedUser&&<>
+         <UserTitleCont/>
+         <UserChatContainer/>
+         <NewChatContainer />
+         </>
+      }
+      </section>
+  
+      
+      :
+      
+      <section className={styles.outerCont} style={{display:chatData.chatdisplay===true?"flex":"none",flexDirection:"column"}}>
+      {!chatData.selectedUser&&<><div className={styles.innerCont}>No Chat Selected</div></>}
+      {chatData.selectedUser&&<>
+         <UserTitleCont/>
+         <UserChatContainer/>
+         <NewChatContainer />
+      </>}
+      </section>
+  
+      }
+      </>
   )
 }
 
--- src/pages/Chat/User Title Cont/UserTitleCont.js
+++ src/pages/Chat/User Title Cont/UserTitleCont.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import styles from "./UserTitleCont.module.css"
-import {BiDotsVerticalRounded} from "react-icons/bi"
-import { useSelector } from 'react-redux'
+import { BiDotsVerticalRounded } from "react-icons/bi"
+import { AiOutlineClose } from "react-icons/ai"
+import { useSelector, useDispatch } from 'react-redux'
+import { updateSelectedUser,Chatshow } from '../../../features/chatSlice_latest'
 
 const UserTitleCont = () => {
-  const chatData=useSelector((state)=>state.chatLatest)
-  
+  const chatData = useSelector((state) => state.chatLatest)
+  const dispatch = useDispatch()
+
   return (
     <section className={styles.outerCont}>
-        <img className={styles.userImg} src={chatData.selectedUser.userImg} alt="userImg" />
+      <img className={styles.userImg} src={chatData.selectedUser.userImg} alt="userImg" />
 
-        <div className={styles.userNameNStatus}>
-            <h3 className={styles.userName}>{chatData.selectedUser.name}</h3>
-            {/* <p className={styles.userStatus}>Online</p> */}
-        </div>
+      <div className={styles.userNameNStatus}>
+        <h3 className={styles.userName}>{chatData.selectedUser.name}</h3>
+        {/* <p className={styles.userStatus}>Online</p> */}
+      </div>
 
-        <BiDotsVerticalRounded className={styles.infoIcon}/>
+      <BiDotsVerticalRounded className={styles.infoIcon} />
+      <AiOutlineClose onClick={() => {
+        dispatch(updateSelectedUser(null))
+        dispatch(Chatshow())
+      }} />
     </section>
   )
 }
