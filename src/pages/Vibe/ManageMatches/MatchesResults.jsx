d4c57609e0f415800c5d4493d68a54bfdc9fbcd7
ChaudharyRaman <ramanchaudhary2704@gmail.com>
2023-08-03 08:24:00 +0530

    match & likedUser functionality

--- src/pages/Vibe/ManageMatches/MatchesResults.jsx
+++ src/pages/Vibe/ManageMatches/MatchesResults.jsx
@@ -1,41 +1,66 @@
-import React from 'react'
-import style from "./matches.module.css"
+import React from "react";
+import style from "./matches.module.css";
+import { useNavigate } from "react-router-dom";
 
-const MatchesResults = ({data,ismanage}) => {
+const MatchesResults = ({ data, ismanage }) => {
+  const navigate = useNavigate();
+  const HandleRemoveLikedUser = () => {};
+  const HandleRemoveMatchUsers = () => {};
   return (
     <div className={style.matchesreusultmiddlecontainer}>
-       
-        {
-            data.map((item,key)=>(
-                <div className={style.matchesresultinnercontainer}>
-                    
-                    <div className={style.singlematchresultcontainer} key={key}>
-                <img style={{width:"35px",height:"35px"}} src={item.img} alt="img" />
-                <div>
-                <p>{item.name}</p>
-                <p style={{fontSize:"15px",color:"#A7A7A7"}}>{item.designation }</p>
-                </div>
-                
-              </div>
+      {data.map((item, key) => (
+        <div className={style.matchesresultinnercontainer}>
+          <div className={style.singlematchresultcontainer} key={key}>
+            <img
+              style={{ width: "35px", height: "35px" }}
+              src={item.image}
+              alt="img"
+            />
+            <div>
+              <p>{item.name}</p>
+              <p style={{ fontSize: "15px", color: "#A7A7A7" }}>
+                {item.designation}
+              </p>
+            </div>
+          </div>
 
-              <div className={style.buttonContainer}>
-                {
-                    ismanage ?  <button style={{background:"none",border:"2px solid #00B3FF",color:"#00B3FF"}}>Remove</button>
-                    :
-                    <div>
-                    <button>View Profile</button>
-                    <span>x</span>
-                    </div>
-                }
-     
-               
+          <div className={style.buttonContainer}>
+            {ismanage ? (
+              <button
+                style={{
+                  background: "none",
+                  border: "2px solid #00B3FF",
+                  color: "#00B3FF",
+                }}
+                onClick={() => {
+                  HandleRemoveMatchUsers();
+                }}
+              >
+                Remove
+              </button>
+            ) : (
+              <div>
+                <button
+                  onClick={() => {
+                    navigate(`/userprofile/${item?.email}`);
+                  }}
+                >
+                  View Profile
+                </button>
+                <span
+                  onClick={() => {
+                    HandleRemoveLikedUser();
+                  }}
+                >
+                  x
+                </span>
               </div>
-
-                </div>
-            ))
-        }
+            )}
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default MatchesResults
+export default MatchesResults;
