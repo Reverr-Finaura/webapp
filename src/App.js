a83ca0e261bfe1418c60320d3165c385ba94d40f
Rishab29-code <rihandev36@gmail.com>
2023-08-10 17:36:57 +0530

    LeftCont responsive

--- src/App.js
+++ src/App.js
@@ -144,6 +144,7 @@ import VibeOuter from "./pages/Vibe/Left/LeftContainer";
 function App() {
   const user = useSelector(selectUser);
   const newUser = useSelector(selectNewUser);
+ const onBoardingData=useSelector((state)=>state.onboarding)
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -415,9 +416,9 @@ function App() {
         ></Route>
         <Route path="/discover/finance" element={<Finance />}></Route>
         <Route path="/discover/healthcare" element={<Healthcare />}></Route>
-        <Route path="/vibetestinga" element={<VibeTestA />}></Route>
+        <Route path="/vibetestinga" element={(onBoardingData?.vibeuser===false)?(<Navigate replace to={"/vibe"}/>):(<VibeTestA />)}></Route>
         {/* <Route path="/vibetestingb" element={<FilterPart />}></Route> */}
-        <Route path="/vibe" element={<VibeOuter />}></Route>
+       <Route path="/vibe" element={(onBoardingData?.vibeuser===true) ?<Navigate replace to={"/vibetestinga"}/> :  <VibeOuter />}></Route>
       </Routes>
     </>
   );
