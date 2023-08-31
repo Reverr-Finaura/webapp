feaaf106616eb536030bd274e4b54e089099bc3f
siva tharun <sivatharun2212@gmail.com>
2023-08-31 18:27:24 +0530

    changes in blog logout

--- webapp-updated-master/src/ProtectedRoute.js
+++ webapp-updated-master/src/ProtectedRoute.js
@@ -2,17 +2,13 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const ProtectedRoute = ({ children }) => {
-  // const blogUser = useSelector((state) => state.blogUser);
-  // if (!blogUser.blogUser) {
-  //   return <Navigate to="/blogsignin" replace />;
-  // }
-
-  // return children;
-  const storedBlogUser = localStorage.getItem("blogUser");
-  if(storedBlogUser === null){
+  const blogUser = useSelector((state) => state.blogUser);
+  if (blogUser.blogUser === null) {
     return <Navigate to="/blogsignin" replace />;
   }
-  return children
+  
+
+  return children;
 };
 
 export default ProtectedRoute;
